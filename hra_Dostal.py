82b845ccbf75f50b08188eecc96885fdd1860d15
Filip Dostál <dos41071@spseol.cz>
2023-10-26 10:30:42 +0200

    První commit

---
+++ hra_Dostal.py
@@ -0,0 +1,104 @@
+import time
+import random
+
+def uvod():
+    print("Vítej v textové hře 'Útěk ze školy'!")
+    print("Nacházíš se ve třídě a tvůj úkol je ze školy utéct a při nejlpepším se vyhnout všem učitelům.")
+    print("Při průchodu hrou se budeš muset rozhodovat a tvoje rozhodnutí budou mít dopad na to jak hra skončí")
+    print("Buď opatrný a správně vybírej, abys se ze školy dostal v bezpečí a bez trestu.")
+
+def konec():
+    print("Děkuji za hraní hry 'Útěk ze školy'!")
+
+def zacatek_hry():
+    uvod()
+    volba = input("Co uděláš? Půjdeš k oknu (okno) nebo ke dveřím (dveře)?").lower()
+    if "dveře" in volba:
+        dveře()
+    elif "okno" in volba:
+        okno()
+    else:
+        print("Nerozumím tvému příkazu, zkuste to znovu.")
+
+def okno():
+    print("Šel jsi k oknu, ale je bohužel zamčené.")
+    volba = input("Budeš klíč hledat (hledat klíč), nebo spíš zkusíš dveře (jít ke dveřím)?").lower()
+    if "hledat klíč" in volba:
+        print("Našel jsi klíč a jsi osvobozen ze školy!")
+        print("Konec hry")
+        konec()
+    elif "jít ke dveřím" in volba:
+        dveře()
+    else:
+        print("Nerozumím tvému příkazu, zkuste to znovu.")
+
+def dveře():
+    print("Jdeš ke dveřím a ty byli poněkud záhadně odemčené.")
+    print("Jdeš na chodbu.")
+    chodba()
+
+def chodba():
+    print("Na chodbě ale potkáš paní učitelku Matlerovou.")
+    print("Ta když tě uvidí tak ti hned zadá ať jí napíšeš slohovku.")
+    volba = input("Půjdeš si napsat slohovku (napsat slohovku), nebo se jí pokusíš utýct (útěk)?").lower()
+    if "napsat slohovku" in volba:
+        slohovka()
+    if "útěk" in volba:
+        cislo = random.randint(1,2)
+        if 1 in cislo:
+            print("Podařilo se ti před paní učitelkou utýct a jdeš tedy k šatnám.")
+            šatny()
+        if 2 in cislo:
+            print("Bohužel se ti před paní učitelkou nepodařilo utýct a chytila tě.")
+            slohovka()
+    else:
+        print("Nerozumím tvému příkazu, zkuste to znovu.")
+
+def slohovka():
+        print("Paní učitelka tě odvede do jedné ze tříd, kde si budeš muset tu slohovku napsat.")
+        print("Tak se do toho vrhneš.")
+        time.sleep(2)
+        print("Pořád píšeš......")
+        time.sleep(2)
+        print("Ta slohovka má mít 250 slov, tak si s ní chceš dát záležet.")
+        time.sleep(2)
+        print("Když už tu slohovku konečně dopíšeš, tak tě paní učitelka nechá konečně na pokoji.")
+        print("Jdeš tedy k šatnám.")
+        šatny()
+
+def šatny():
+    print("U šaten bohužel potkáš pana školníka.")
+    print("Ten ti řekne aby sis sundal boty, protože prý 'děláš neskutečný bordel', nebo že si ten svinčík máš uklidit.")
+    volba = input("Sundáš si boty (sundat boty), nebo si radši ten nepořádek uklidíš (uklidit)?").lower()
+    if "sundat boty" in volba:
+        print("Sundal sis boty a jdeš naboso až k východu ze školy.")
+        východ()
+    if "uklidit" in volba:
+        print("Pan školník ti dá lopatku a koště a ty si musíš uklidit všechno co ti z bot spadalo a možná i to co spadalo spolužákům.")
+        print("Když to douklízíš, pan školník tě nechá jít.")
+        východ()
+    else:
+        print("Nerozumím tvému příkazu, zkuste to znovu.")
+
+
+def východ():
+    print("U východu ale potkáš další a teď snad už poslední nemilou návštěvu.")
+    print("Před tebou se objeví obávaný pan učitel Šmehlík.")
+    print("Ten chce po tobě aby jsi udělal 10 kliků.")
+    volba = input("Uděláš těch 10 kliků (kliky), nebo se radši pokusíš utýct (útěk)?")
+    if "kliky" in volba:
+        kliky()
+    if "útěk" in volba:
+        cislo = random.randint(1,2)
+        if 1 in cislo:
+            print("Podařilo se ti před Šmehlíkem utýct a tím pádem jsi unikl ze školy.")
+            konec()
+        if 2 in cislo:
+            print("Nepodařilo se ti před Šmehlíkem utýct, a ty kliky si tím pádem budeš muset udělat.")
+            kliky()
+
+def kliky():
+        print("Ty si teda přemůžeš a hezky to před ním odklikuješ.")
+        print("Po klicích jsi trochu unavený, ale pořád se zvládneš zvednout a teď už v klidu odejít ze školy.")
+        konec()
+
